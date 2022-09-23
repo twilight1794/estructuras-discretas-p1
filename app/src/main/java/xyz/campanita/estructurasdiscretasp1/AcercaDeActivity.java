0f16200153a077007a451ae1d32e9ed9e45ead7a
Giovanni Alfredo Garciliano Díaz <giobeatle10@gmail.com>
2022-09-23 01:28:48 -0500

    Avance hasta 22 sep - 2
    * Temas de ED
    * Solucionar muchos problemas en el codigo
    * QUitar SVG problematico

--- app/src/main/java/xyz/campanita/estructurasdiscretasp1/AcercaDeActivity.java
+++ app/src/main/java/xyz/campanita/estructurasdiscretasp1/AcercaDeActivity.java
@@ -2,7 +2,10 @@ package xyz.campanita.estructurasdiscretasp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class AcercaDeActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class AcercaDeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        ImageView im = (ImageView) findViewById(R.id.img_fi);
+        im.setOnClickListener(v -> {
+            Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ingenieria.unam.mx/"));
+            startActivity(in);
+        });
+        im = (ImageView) findViewById(R.id.img_unam);
+        im.setOnClickListener(v -> {
+            Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.unam.mx/"));
+            startActivity(in);
+        });
     }
 }
--- app/src/main/java/xyz/campanita/estructurasdiscretasp1/MainActivity.java
+++ app/src/main/java/xyz/campanita/estructurasdiscretasp1/MainActivity.java
@@ -7,57 +7,35 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.widget.Button;
 
 import xyz.campanita.estructurasdiscretasp1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        xyz.campanita.estructurasdiscretasp1.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
-        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
+        Button button = (Button) findViewById(R.id.mostrar_todos_l);
+        button.setOnClickListener(v -> Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        button = (Button) findViewById(R.id.mostrar_todos_t);
+        button.setOnClickListener(v -> Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        button = (Button) findViewById(R.id.preferencias);
+        button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AjustesActivity.class)));
+
+        button = (Button) findViewById(R.id.acercade);
+        button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AcercaDeActivity.class)));
+
+        binding.fab.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, BusquedaActivity.class)));
     }
-
-    public void lanzarAjustes(View view){
-        Intent i = new Intent(this, AjustesActivity.class);
-        startActivity(i);
-    }
-
-    public void lanzarAcercaDe(View view){
-        Intent i = new Intent(this, AcercaDeActivity.class);
-        startActivity(i);
-    }
-
-
-    /*@Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }*/
 }
