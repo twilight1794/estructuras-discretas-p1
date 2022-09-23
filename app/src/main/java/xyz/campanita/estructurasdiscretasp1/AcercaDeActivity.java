b3d3a6c83d1b1f7d788e4cfa24af3df6e7149d14
Giovanni Alfredo Garciliano Díaz <giobeatle10@gmail.com>
2022-09-23 12:33:26 -0500

    Avance hasta 23 sep
    * Colocar AppBar en todas las actividades

--- app/src/main/java/xyz/campanita/estructurasdiscretasp1/AcercaDeActivity.java
+++ app/src/main/java/xyz/campanita/estructurasdiscretasp1/AcercaDeActivity.java
@@ -7,12 +7,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import xyz.campanita.estructurasdiscretasp1.databinding.ActivityAcercaDeBinding;
+import xyz.campanita.estructurasdiscretasp1.databinding.ActivityMainBinding;
+
 public class AcercaDeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acerca_de);
+
+        xyz.campanita.estructurasdiscretasp1.databinding.ActivityAcercaDeBinding binding = ActivityAcercaDeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
 
         ImageView im = (ImageView) findViewById(R.id.img_fi);
         im.setOnClickListener(v -> {
